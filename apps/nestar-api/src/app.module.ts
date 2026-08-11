f35ae61b095f856180ae6958d0fdce956b7e148e
matt <khikmat64168@gmail.com>
2026-08-11 16:12:16 +0900

    fix: connect MongoDb database

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
 	imports: [
@@ -14,6 +16,8 @@ import { AppResolver } from './app.resolver';
 			playground: true,
 			autoSchemaFile: true,
 		}),
+		ComponentsModule,
+		DatabaseModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
