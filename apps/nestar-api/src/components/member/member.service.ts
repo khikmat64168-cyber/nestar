0c4979e03a67ccfb6c1cdb353a8d24e8b2fd92b4
matt <khikmat64168@gmail.com>
2026-08-13 07:38:22 +0900

    fix: signup business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
-	public async signup(): Promise<string> {
-		return 'Signup executed';
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	public async signup(input: MemberInput): Promise<Member> {
+		//Hashing password
+		try {
+			const result = await this.memberModel.create(input);
+			//Auth via tokens
+			return result;
+		} catch (err) {
+			console.log('Error servicemodel: ', err);
+			if (err instanceof Error && 'code' in err && err.code === 11000) {
+				throw new BadRequestException('Member already exists with this phone number or nickname');
+			}
+			throw new BadRequestException();
+		}
 	}
 
 	public async login(): Promise<string> {
