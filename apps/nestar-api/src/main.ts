368693d7f6232bb2c1e5ee80b674592cc0157c86
matt <khikmat64168@gmail.com>
2026-08-18 12:45:59 +0900

    feat: standard integration of logging interceptors

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptors/Logging.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	//validation pipe ni ulash uchun global integraton
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new LoggingInterceptor());
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
