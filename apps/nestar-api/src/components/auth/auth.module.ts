e936a751b435583d03503267e0411202a4515ebc
matt <khikmat64168@gmail.com>
2026-08-20 15:11:22 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/auth/auth.module.ts
+++ apps/nestar-api/src/components/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 
 @Module({
+	imports: [
+		HttpModule,
+		JwtModule.register({ secret: `${process.env.SECRET_TOKEN}`, signOptions: { expiresIn: '30d' } }),
+	],
 	providers: [AuthService],
 	exports: [AuthService],
 })
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -19,6 +19,8 @@ export class MemberService {
 		try {
 			const result = await this.memberModel.create(input);
 			//Auth via tokens
+			result.accessToken = await this.authService.createToken(result);
+
 			return result;
 		} catch (err) {
 			console.log('Error servicemodel: ', err instanceof Error ? err.message : String(err));
@@ -45,6 +47,9 @@ export class MemberService {
 
 		const isMatch = await this.authService.comparePassword(input.memberPassword, response.memberPassword ?? '');
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+
+		delete response.memberPassword;
+		response.accessToken = await this.authService.createToken(response);
 
 		return response;
 	}
