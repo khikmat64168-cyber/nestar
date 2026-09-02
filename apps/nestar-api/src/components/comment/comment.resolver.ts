b0473e3df7ecbaac1e81a057f2ffc2e5d3c1d47f
matt <khikmat64168@gmail.com>
2026-09-03 03:20:26 +0900

    feat: develop removeCommentByAdmin Graph ql API s

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -9,6 +9,9 @@ import { Comment, Comments } from '../../libs/dto/member/comment/comment';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { CommentUpdate } from '../../libs/dto/member/comment/comment.update';
 import { WithoutGuard } from '../auth/guards/without.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberType } from '../../libs/enums/member.enums';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Resolver()
 export class CommentResolver {
@@ -45,5 +48,16 @@ export class CommentResolver {
 		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
 		const result = await this.commentService.getComments(memberId, input);
 		return result;
+	}
+
+	/** ADMIN **/
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Comment)
+	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+		console.log('Mutation: removeCommentByAdmin');
+		const commentId = shapeIntoMongoObjectId(input);
+		return await this.commentService.removeCommentByAdmin(commentId);
 	}
 }
