5c33b9cc6d9b156a24faa60a36a310bc54ec95ec
matt <khikmat64168@gmail.com>
2026-09-05 03:45:14 +0900

    feat: develop likeTarfetMember Graph QL . APi

--- apps/nestar-api/src/libs/dto/member/like/like.input.ts
+++ apps/nestar-api/src/libs/dto/member/like/like.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
-import { ObjectId } from 'mongoose';
-import { LikeGroup } from '../../enums/like.enum';
+import type { ObjectId } from 'mongoose';
+import { LikeGroup } from '../../../enums/like.enum';
 
 @InputType()
 export class LikeInput {
--- apps/nestar-api/src/libs/dto/member/like/like.ts
+++ apps/nestar-api/src/libs/dto/member/like/like.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { LikeGroup } from '../../enums/like.enum';
-import { ObjectId } from 'mongoose';
+import { LikeGroup } from '../../../enums/like.enum';
+import type { ObjectId } from 'mongoose';
 
 @ObjectType()
 export class MeLiked {
