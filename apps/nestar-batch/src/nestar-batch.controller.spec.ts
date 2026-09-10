a9a0bb775ce430971edad4e99af1e3eff22496b9
matt <khikmat64168@gmail.com>
2026-09-10 14:56:18 +0900

    fix: modify batch module and connect toMongoDB

--- apps/nestar-batch/src/nestar-batch.controller.spec.ts
+++ apps/nestar-batch/src/nestar-batch.controller.spec.ts
@@ -1,22 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NestarBatchController } from './nestar-batch.controller';
-import { NestarBatchService } from './nestar-batch.service';
+import { NestarBatchController } from './batch.controller';
+import { NestarBatchService } from './batch.service';
 
 describe('NestarBatchController', () => {
-  let nestarBatchController: NestarBatchController;
+	let nestarBatchController: NestarBatchController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [NestarBatchController],
-      providers: [NestarBatchService],
-    }).compile();
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			controllers: [NestarBatchController],
+			providers: [NestarBatchService],
+		}).compile();
 
-    nestarBatchController = app.get<NestarBatchController>(NestarBatchController);
-  });
+		nestarBatchController = app.get<NestarBatchController>(NestarBatchController);
+	});
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(nestarBatchController.getHello()).toBe('Hello World!');
-    });
-  });
+	describe('root', () => {
+		it('should return "Hello World!"', () => {
+			expect(nestarBatchController.getHello()).toBe('Hello World!');
+		});
+	});
 });
